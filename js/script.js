200d209cfde47974b1fb79fe36c82c8a76c604c2
EldissoneUnione <evilonga@unioneafrica.com>
2025-05-27 16:35:39 +0100

    Atualização do site

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const control = document.getElementById("direction-toggle");
 const marquees = document.querySelectorAll(".marquee");
 const wrapper = document.querySelector(".wrapper");
-
+/*
 control.addEventListener("click", () => {
   control.classList.toggle("toggle--vertical");
   wrapper.classList.toggle("wrapper--vertical");
@@ -9,3 +9,12 @@ control.addEventListener("click", () => {
     marquee.classList.toggle("marquee--vertical")
   );
 });
+*/
+
+const toggleBtn = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+toggleBtn.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
